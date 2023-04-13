a87ec9ddfa066c47be9fe3ed2facec0e32e7bf97
KamilH <kamil.horzela@etagliwice.pl>
2023-04-13 09:42:13 +0200

    feat: rfm_init() draft

--- Core/Src/rfm75.c
+++ Core/Src/rfm75.c
@@ -15,34 +15,44 @@
 #include "rfm75.h"
 
 //***PRIVATE FUNCION DECLARATIONS***//
-uint8_t* spi_transaction(uint8_t cmd, uint8_t* data, uint8_t size);
+uint8_t* spi_transaction(uint8_t cmd, uint8_t* data);
 //**********************************//
 
 
 //***PUBLIC FUNCTIONS***//
 void rfm_init(){
 	HAL_GPIO_WritePin(SPI2_NSS_GPIO_Port, SPI2_NSS_Pin, GPIO_PIN_SET);
-
+	spi_transaction(CMD_W_REGISTER, 0x03);	//PWR_UP, PRX
 }
 
 
 void rfm_transmit(uint8_t* data){
-	spi_transaction(CMD_W_TX_PAYLOAD, data, sizeof(data));
+	spi_transaction(CMD_W_REGISTER, 0x02);	//PWR_UP, PTX
+	spi_transaction(CMD_W_TX_PAYLOAD, data);
 }
 
 
 uint8_t* rfm_receive(uint8_t size){
 	uint8_t tx[size];
-	return spi_transaction(CMD_R_RX_PAYLOAD, tx, size);
+	return spi_transaction(CMD_R_RX_PAYLOAD, tx);
 }
 //**********************//
 
 
 //***PRIVATE FUNCIONS***//
-uint8_t* spi_transaction(uint8_t cmd, uint8_t* data, uint8_t size){
-	uint8_t rx[size];
+uint8_t* spi_transaction(uint8_t cmd, uint8_t* data){
+	uint8_t size = sizeof(data);
+	uint8_t rx[size+1];
+	uint8_t tx[size+1];
+	uint8_t i = 0;
+	tx[0] = CMD_W_REGISTER;
+
+	for(i = 0; i < size + 1; i++){
+		tx[i + 1] = data[i];
+	}
+
 	HAL_GPIO_WritePin(SPI2_NSS_GPIO_Port, SPI2_NSS_Pin, GPIO_PIN_RESET);
-	HAL_SPI_TransmitReceive(&hspi2, data, rx, size, 100);
+	HAL_SPI_TransmitReceive(&hspi2, tx, rx, size+1, 100);
 	HAL_GPIO_WritePin(SPI2_NSS_GPIO_Port, SPI2_NSS_Pin, GPIO_PIN_SET);
 	return *rx;
 }
